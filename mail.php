f666b56f532c188bdeb1681bd471ca68156b08a6
Vishnuraj V <vishnu@flogic.in>
2017-05-27 13:31:33 +0530

    minor fix

--- mail.php
+++ mail.php
@@ -1,34 +1,48 @@
 <?php
 
 require_once "vendor/autoload.php";
-
-//PHPMailer Object
-$mail = new PHPMailer;
-
-//From email address and name
-$mail->From = "vvishnuraj58@gmail.com";
-$mail->FromName = "Vishnuraj V";
-
-//To address and name
-$mail->addAddress("contactus@wissen.com", "Wissen");
-
-//Address to which recipient will reply
-$mail->addReplyTo("vvishnuraj58@gmail.com", "Reply");
-
-//CC and BCC
-
-//Send HTML or Plain Text email
-$mail->isHTML(true);
-
-$mail->Subject = "Subject Text";
-$mail->Body = "<i>Mail body in HTML</i>";
-$mail->AltBody = "This is the plain text version of the email content";
-
-if(!$mail->send())
-{
-    echo "Mailer Error: " . $mail->ErrorInfo;
+$resp = '';
+if(empty($_POST['name']) || empty($_POST['email']) || empty($_POST['subject']) || empty($_POST['message'])) {
+    $resp = 'Please fill all fields.';
 }
-else
-{
-    echo "Message has been sent successfully";
+
+$email = $_POST['email'];
+
+if (filter_var($email, FILTER_VALIDATE_EMAIL) === false) {
+  if($resp==''){
+    $resp = "Please enter a valid email.";
+  }
 }
+
+if($resp ==''){
+    $mail = new PHPMailer;
+
+    //From email address and name
+    $mail->From = $email;
+    $mail->FromName = $_POST['name'];
+
+    //To address and name
+    $mail->addAddress("contactus@wissen.com", "Wissen");
+
+    //Address to which recipient will reply
+    $mail->addReplyTo($email, "Reply");
+
+    //CC and BCC
+
+    //Send HTML or Plain Text email
+    $mail->isHTML(true);
+
+    $mail->Subject = $_POST['subject'];
+    $mail->Body = $_POST['message'];
+    $mail->AltBody = $_POST['message'];
+
+    if(!$mail->send())
+    {
+        $resp = "Mailer Error: " . $mail->ErrorInfo;
+    }
+    else
+    {
+        $resp = "Message has been sent successfully";
+    }
+}
+echo $resp;
